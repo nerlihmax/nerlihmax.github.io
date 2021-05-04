0d9b6a10adac5ec182402ed90f8668266caa7e8a
Nerlih Max <nerlihmax@yandex.ru>
2021-05-04 20:54:00 +0700

    Добавили код страницы

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div class="flex flex-col justify-between h-screen">
+    <Card/>
+    <a class="font-mono m-4 text-blue-700"
+      href="https://github.com/nerlihmax/nerlihmax.github.io">
+      made with &lt;3
+    </a>
+  </div>
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+import Card from './components/Card.vue';
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="flex flex-col justify-start m-4 border border-black
+    p-4 font-mono w-4/6">
+    <h1 class="max-w-max mb-2 text-4xl">
+      {{ profile.fullName }}
+    </h1>
+    <div class="max-w-max">
+      Привет! Я {{ profile.grade }}-разработчик из Новосибирска.
+      Мне {{ profile.age }} лет.
+      Сейчас, я, в основном, занят веб-разработкой на Vue.js.
+      Уже {{ profile.roboticsExpirience }}
+      лет занимаюсь соревновательной робототехникой,
+      участвовал и показывал достойные результаты на
+      соревнованиях международного уровня, таких, как
+      Robocup, Russian Robotic Olympic.
+      Лауреат и победитель множества мероприятий регионального уровня.
+    </div>
+    <div class="flex flex-row justify-start flex-wrap align-middle">
+      <div v-for="(item) in profile.links" :key="item">
+        <a class="text-blue-700 mr-4"
+          :href="item.link">{{ item.name }}</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const profile = ref({
+  fullName: 'Максим Нерлих',
+  grade: 'junior',
+  birthday: new Date('2001-12-27'),
+  age: new Date().getUTCFullYear() - new Date('2001-12-27').getUTCFullYear() - 1,
+  roboticsExpirience: new Date().getUTCFullYear() - 2012,
+  links: [
+    {
+      name: 'Telegram',
+      link: 'https://t.me/nerlihmax',
+    },
+    {
+      name: 'VK',
+      link: 'https://vk.com/nerlihmax',
+    },
+    {
+      name: 'Github',
+      link: 'https://github.com/nerlihmax',
+    },
+    {
+      name: 'Twitch',
+      link: 'https://twitch.tv/nerlihmax',
+    },
+    {
+      name: 'Instagram',
+      link: 'https://instagram.com/nerlihmax',
+    },
+  ],
+});
+</script>
+
+<style scoped>
+</style>
